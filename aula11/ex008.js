d3b1146e0360c145b70643e3f734a1b932b3544b
Rafael Bonenberger <118410209+bonenberger@users.noreply.github.com>
2023-02-16 16:41:57 -0300

    novos exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`a velocidade do seu carro é de ${vel}km/h`)
+if (vel>60) { //condição simples
+    console.log('vc ultrapassou o limite de velocidade da via e foi multado')
+}
+console.log(`dirija sempre com segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'brasil'
+if (pais == 'brasil') {
+    console.log('vc é brasileiro')
+} else if (pais != 'brasil') {
+    console.log('vc é extrangeiro')
+}
